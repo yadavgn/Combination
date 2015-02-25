dba32241341ba95dba20bd1b0c50e7cead049c76
yadavgn <yadav.ghanshyam@gmail.com>
2015-02-25 12:21:28 +0530

    initial project to find combinations

---
+++ Combinations/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Combinations
+{
+    class Program
+    {
+        private static int count;
+        static void Main(string[] args)
+        {
+            count = 1;
+            printCombinations("AB", 0,"");
+
+            Console.ReadLine();
+        }
+
+        static void printCombinations(String input, int index, String combination)
+        {
+            if (combination.Length == input.Length)
+            {
+                Console.WriteLine("{0} , {1}", count++, combination);
+                return;
+            }
+            foreach (var _char in input)
+            {
+                printCombinations(input, index + 1, combination + _char);
+            }
+        }
+    }
+}
